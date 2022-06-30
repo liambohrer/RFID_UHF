9687262fc9eba7f370ac323b009d6deb8b8deb40
liam bohrer <liam.bohrer@etu.umontepellier.fr>
2022-06-30 18:26:04 +0200

    first demo

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import subprocess
 import time
 
 import EM4325
-import test
 
-COMMAND = "D:\\Users\\Gabin\\OneDrive\\Documents\\Cours\\S10\\RFID\\CS\\cs\\Samples\\Codelets\\Read\\bin\\Debug\\Read.exe"
-
+PORT_COM = 17
 
 def uhf_read(testing: bool = False):
     devices = []
@@ -16,8 +14,7 @@ def uhf_read(testing: bool = False):
         with open('test_read_uhf', 'r') as file:
             data = file.read().rstrip()
     else:
-        res = subprocess.run([COMMAND, "tmr:///com4", "--ant 1"], capture_output=True, text=True)
-        data = res.stdout
+        data = subprocess.check_output(os.path.abspath(os.getcwd()) + "\\Read.exe tmr:///com" + str(PORT_COM) + " --ant 1", shell=True, text=True)
 
     if (match := re.findall(EM4325.REGEX, data)) is not None:
         for m in match:
@@ -27,15 +24,33 @@ def uhf_read(testing: bool = False):
     return devices
 
 
+def uhf_leds(tag_id: int, led1: bool, led2: bool):
+    cmd = os.path.abspath(os.getcwd()) + \
+    "\\Leds.exe tmr:///com" + str(PORT_COM) + \
+          " --ant 1 --led1 " + str(int(led1)) + \
+          " --led2 " + str(int(led2)) + \
+          " --epc " + str(tag_id)
+    
+    print(cmd)
+    subprocess.check_output(cmd,
+          shell=True, text=True)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    toggle = True
     while True:
+        toggle = not toggle
+        devices = uhf_read(testing=False)
         print("\n==================================================")
         print("Read UHL: ")
-        devices = uhf_read(testing=True)
         n = 0
         for device in devices:
             print("Tag id n°" + str(n) + ": " + str(device.tag_id))
+            if n % 2 == 1:
+                uhf_leds(device.tag_id, toggle, not toggle)
+            else:
+                uhf_leds(device.tag_id, not toggle, toggle)
             n += 1
         print("==================================================\n")
         time.sleep(1)
